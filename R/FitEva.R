973e65bfc50f53173b37d227fd9671ad529942d7
QIN Yiming <yimingdan@gmail.com>
2018-12-06 17:04:46 +0800

    1. Convert Beta (bot the fitted one and the known one) to matrix in case it is in data.frame format. 2. Make cutoff.sc and cutoff.g only used as cutoffs of single coefficients or groups in the fitted Beta matrix to be positive, not used to the known Beta matrix. 3. Correct the spell of "precision" (wrongly spelt as "pricision") in the returned items.

--- R/FitEva.R
+++ R/FitEva.R
@@ -7,8 +7,8 @@
 #' @param Beta.simulate The simulated or golden standarded \code{B} matrix.
 #' @param Beta.fit The fitted \code{B} matrix using \code{\link{PgaMsgl}} or other algorithms.
 #' @param Gm Matrix of the group structure of coefficient matrix \code{B}. It is the a matrix of group boundaries, with each row indicating a group, four columns indicate the row-start, row-end, column-start and column-end of the group. The row/column index is 1-based.
-#' @param cutoff.sc A number indicates the cutoff of a single coefficient in \code{B} to be true positive.
-#' @param cutoff.g A number indicates the cutoff of the norm of a group (a sub-matrix in truth) in \code{B} to be true positive.
+#' @param cutoff.sc A non-negative number indicates the cutoff of a single coefficient in \code{B} to be positive in the fitted \code{B} matrix.
+#' @param cutoff.g A non-negative number indicates the cutoff of the norm of a group (a sub-matrix in truth) in the fitted \code{B} matrix to be positive.
 #' 
 #' @return \item{sc.TPR}{The true positive rate of single coefficients prediction.}
 #' @return \item{sc.FPR}{The false positive rate of single coefficients prediction.}
@@ -36,15 +36,15 @@ FitEva <- function(Beta.simulate, Beta.fit, Gm, cutoff.sc, cutoff.g)
   if(cutoff.sc < 0) {
     stop("Please indicate a non-negative cutoff of coefficient.")
   }
-  TP.sc = sum( which( abs(Beta.fit) > cutoff.sc ) %in% which( abs(Beta.simulate) > cutoff.sc ))
-  TTP.sc = sum( Beta.fit*Beta.simulate > cutoff.sc^2 )
-  FP.sc = sum(! which( abs(Beta.fit) > cutoff.sc ) %in% which( abs(Beta.simulate) > cutoff.sc ))
+  TP.sc = sum( which( abs(Beta.fit) > cutoff.sc ) %in% which( abs(Beta.simulate) > 0 ))
+  TTP.sc = sum( Beta.fit*Beta.simulate > 0 )
+  FP.sc = sum(! which( abs(Beta.fit) > cutoff.sc ) %in% which( abs(Beta.simulate) > 0 ))
   P.sc = sum( abs(Beta.fit) > cutoff.sc )
   
-  TN.sc = sum(which( abs(Beta.fit) <= cutoff.sc ) %in% which( abs(Beta.simulate) <= cutoff.sc ))
+  TN.sc = sum(which( abs(Beta.fit) <= cutoff.sc ) %in% which( abs(Beta.simulate) <= 0 ))
   
-  CP.sc = sum(abs(Beta.simulate) > cutoff.sc)
-  CN.sc = sum(abs(Beta.simulate) <= cutoff.sc)
+  CP.sc = sum(abs(Beta.simulate) > 0)
+  CN.sc = sum(abs(Beta.simulate) <= 0)
   
   TPR.sc = TP.sc / CP.sc
   TTPR.sc = TTP.sc / CP.sc
@@ -62,16 +62,16 @@ FitEva <- function(Beta.simulate, Beta.fit, Gm, cutoff.sc, cutoff.g)
   
   for (i in 1:n.g)
   {
-    simulate.g.norm[i]=norm(Beta.simulate[Gm[i,1]:Gm[i,2],Gm[i,3]:Gm[i,4]] , 'F')
-    try.g.norm[i]=norm(Beta.fit[Gm[i,1]:Gm[i,2],Gm[i,3]:Gm[i,4]] , 'F')
+    simulate.g.norm[i]=norm(as.matrix(Beta.simulate[Gm[i,1]:Gm[i,2],Gm[i,3]:Gm[i,4]]) , 'F')
+    try.g.norm[i]=norm(as.matrix(Beta.fit[Gm[i,1]:Gm[i,2],Gm[i,3]:Gm[i,4]]) , 'F')
   }
   
-  TP.g = sum(which(try.g.norm > cutoff.g) %in% which(simulate.g.norm > cutoff.g))
-  FP.g = sum(! which(try.g.norm > cutoff.g) %in% which(simulate.g.norm > cutoff.g))
+  TP.g = sum(which(try.g.norm > cutoff.g) %in% which(simulate.g.norm > 0))
+  FP.g = sum(! which(try.g.norm > cutoff.g) %in% which(simulate.g.norm > 0))
   P.g = sum(try.g.norm > cutoff.g)
-  TN.g = sum(which(try.g.norm <= cutoff.g) %in% which(simulate.g.norm <= cutoff.g))
-  CP.g = sum(simulate.g.norm > cutoff.g)
-  CN.g = sum(simulate.g.norm <= cutoff.g)
+  TN.g = sum(which(try.g.norm <= cutoff.g) %in% which(simulate.g.norm <= 0))
+  CP.g = sum(simulate.g.norm > 0)
+  CN.g = sum(simulate.g.norm <= 0)
   
   TPR.g = TP.g / CP.g
   FPR.g = FP.g / CN.g
@@ -82,12 +82,12 @@ FitEva <- function(Beta.simulate, Beta.fit, Gm, cutoff.sc, cutoff.g)
   return(list(sc.TPR=TPR.sc, 
               sc.FPR=FPR.sc, 
               sc.TTPR=TTPR.sc, 
-              sc.pricision=precision.sc, 
+              sc.precision=precision.sc, 
               sc.accuracy=accuracy.sc, 
               sc.F1=F1.sc,
               g.TPR=TPR.g, 
               g.FPR=FPR.g, 
-              g.pricision=precision.g, 
+              g.precision=precision.g, 
               g.accuracy=accuracy.g, 
               g.F1=F1.g))
 }
